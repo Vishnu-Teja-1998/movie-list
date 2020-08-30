0d958e5906d3d565a1d05505000d43b097bf5421
vishnu <chilamakurvishnu@gmail.com>
2020-08-30 16:00:18 +0530

    functonality added to ADD MOVIE button

---
+++ assets/scripts/app.js
@@ -0,0 +1,9 @@
+const addMovieModal = document.getElementById("add-modal");
+
+const startAddMovieButton = document.querySelector("header button");
+
+const toggleMovieModel = () => {
+  addMovieModal.classList.toggle("visible");
+};
+
+startAddMovieButton.addEventListener("click", toggleMovieModel);
