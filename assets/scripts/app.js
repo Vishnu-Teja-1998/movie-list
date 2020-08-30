af21b122332aed1d583311818a3cf6d2b22eae61
vishnu <chilamakurvishnu@gmail.com>
2020-08-30 16:32:46 +0530

    functonality added to cancel button and overlay

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,8 +2,27 @@ const addMovieModal = document.getElementById("add-modal");
 
 const startAddMovieButton = document.querySelector("header button");
 
+const backdrop = document.getElementById("backdrop");
+
+const cancelAddMovieButton = document.querySelector(".btn--passive");
+
+const toggleBackdrop = () => {
+  backdrop.classList.toggle("visible");
+};
+
 const toggleMovieModel = () => {
   addMovieModal.classList.toggle("visible");
+  toggleBackdrop();
+};
+
+const cancelAddMovie = () => {
+  toggleMovieModel();
+};
+
+const backdropClickHandler = () => {
+  toggleMovieModel();
 };
 
 startAddMovieButton.addEventListener("click", toggleMovieModel);
+backdrop.addEventListener("click", backdropClickHandler);
+cancelAddMovieButton.addEventListener("click", cancelAddMovie);
